35e19e9a5649903415eea15189efa17d09f17737
Levi Siuzdak <levi@lvsz.dk>
2023-05-21 21:23:29 +0200

    Added Russia & Argentina CSVs
    Signed-off-by: Levi Siuzdak <levi@lvsz.dk>

--- bokeh_examples.py
+++ bokeh_examples.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import random
 import pandas as pd
 import nasdaqdatalink as quandl
 from bokeh.plotting import figure, show, output_file
@@ -19,6 +20,8 @@ zones = {
     'AUS': 'AUS', 
     'CAN': 'CAN', 
     'NZL': 'NZL',
+    'RUS': 'RUS',
+    'ARG': 'ARG',
     }
 
 bigmac_data = pd.read_csv("data/bigmac/adjusted-index.csv")
@@ -46,13 +49,40 @@ p = figure(
 )
 
 # add multiple renderers
-p.line(x, y1, legend_label="USA", color="red", line_width=2)
-p.line(bigmac['AUS'].keys(), bigmac['AUS'], legend_label="Australia",
-       color="yellow", line_width=2)
-p.line(bigmac['EUZ'].keys(), bigmac['EUZ'], legend_label="Euro",
-       color="blue", line_width=2)
-p.line(bigmac['JPN'].keys(), bigmac['JPN'], legend_label="Japan",
-       color="green", line_width=2)
+#p.line(x, y1, legend_label="USA", color="red", line_width=2)
+#p.line(bigmac['AUS'].keys(), bigmac['AUS'], legend_label="Australia",
+#       color="yellow", line_width=2)
+#p.line(bigmac['EUZ'].keys(), bigmac['EUZ'], legend_label="Euro",
+#       color="blue", line_width=2)
+#p.line(bigmac['JPN'].keys(), bigmac['JPN'], legend_label="Japan",
+#       color="green", line_width=2)
+def byte_gen(seed=100, lim=30_000, rand=False):
+    if rand:
+        random.seed(seed)
+        while lim > 0:
+            lim -= 1
+            yield random.randint(0, 0xFF)
+    else:
+        while lim > 0:
+            lim -= 1
+            yield seed
+            seed = (seed + 107) % 0x100
 
-# show the results
-show(p)
+def color_gen(gen=byte_gen()):
+    cont = True
+    while cont:
+        try:
+            rgb = tuple(gen.send(None) for _ in range(3))
+            yield f"rgb{rgb}"
+        except:
+            cont = False
+
+gen_color = lambda: color_gen().send(None)
+
+for cpi, mac in zones.items():
+    p.line(bigmac[mac].keys(), bigmac[mac],
+           legend_label=cpi, color=gen_color(), line_width=3)
+
+if __name__ == '__main__':
+       # show the results
+       show(p)
