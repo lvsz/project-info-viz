0d4cc1c6fc80a0fce1975b84d90e2a24b1057bff
ilyas <ilyas.said.makari@vub.be>
2023-05-24 20:56:01 +0200

    adding a country selector

--- dashboard/script.js
+++ dashboard/script.js
@@ -105,10 +105,12 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
   function initDashboard() {
     let dates = [...new Set(RAW_INDEX.map((row) => row.date))];
+    let countries = [...new Set(RAW_INDEX.map((row) => row.name))];
     chosenDate = dates[0];
 
     initTimeSlider(dates);
     mapChart = initMap();
+    initCountrySelector(countries, chosenCountry);
     currencyValueChart = initCurrencyValueChart();
     correlationChart = initCorrelationChart();
     BMorCPIChart = initIndividualChart();
@@ -433,6 +435,24 @@ document.addEventListener('DOMContentLoaded', function(event) {
         createCorrelationValuesList(correlationList, 'dollar_price');
 
     correlationChart.update();
+  }
+
+  function initCountrySelector(countries, defaultCountry) {
+    let selector = document.querySelector('#countrySelector');
+    countries.sort().forEach((country) => {
+      var opt = document.createElement('option');
+      opt.value = country;
+      opt.innerHTML = country;
+      selector.appendChild(opt);
+    });
+    selector.value = defaultCountry;
+
+    selector.addEventListener("change", () => {
+      console.log(selector.value);
+      chosenCountry = selector.value;
+      changeData();
+    });
+
   }
 
   function initMap() {
